9c8fe86a66d58634eb6b3c932fe680c09a1007a3
Vitalii Horbnko <snaketsanders5@gmail.com>
2021-02-10 22:06:35 +0200

    lesson 22, 23, 24, 25 finished

---
+++ lesson-25/js/index.js
@@ -0,0 +1,43 @@
+//To string
+console.log(typeof String(null));
+
+// 2
+console.log(typeof (null + "5"));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + "px";
+
+console.log(fontSize);
+
+// To number
+
+console.log(typeof Number("4"));
+console.log(typeof +"4");
+
+console.log(typeof parseInt("15px", 10));
+
+let answer = +prompt("Hello", "");
+
+// To boolean
+
+// 0, "", null, undefined, NaN
+// 1)
+let switcher = null;
+if (switcher) {
+  console.log("Working...");
+}
+
+switcher = 1;
+if (switcher) {
+  console.log("Working...");
+}
+
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!'44444'));
