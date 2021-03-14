5a2b140e0b636a531ff36233802eab3618b61830
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-03-14 13:47:10 +0200

    finished lessons

---
+++ lesson-40/js/index.js
@@ -0,0 +1,27 @@
+// const now = new Date("2021-02-09");
+
+// console.log(now.setHours(40));
+// console.log(now);
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+
+// console.log(now.getTime());
+
+// let start = new Date();
+
+// for (let i = 0; i < 100000; i++) {
+//   let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(`Цикл отработал за ${end - start} милисекунд`)
+
+const now = new Date();
+console.log(now.getDay());
