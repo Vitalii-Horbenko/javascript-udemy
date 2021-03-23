e66629011e01b3e7ff50c693cc5d1130efe78314
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-03-23 11:05:33 +0200

    lesson 49 finished. rest operator

--- lesson-48/js/index.js
+++ lesson-48/js/index.js
@@ -140,12 +140,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // Используем классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -157,8 +158,14 @@ window.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement("div");
-      element.innerHTML = `<div class="menu__item">
-      <img src=${this.src} alt=${this.alt} />
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
+      element.innerHTML = `<img src=${this.src} alt=${this.alt} />
       <h3 class="menu__item-subtitle">${this.title}</h3>
       <div class="menu__item-descr">
         ${this.descr}
@@ -167,8 +174,7 @@ window.addEventListener("DOMContentLoaded", () => {
       <div class="menu__item-price">
         <div class="menu__item-cost">Цена:</div>
         <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-      </div>
-    </div>`;
+      </div>`;
 
       this.parent.append(element);
     }
@@ -182,7 +188,9 @@ window.addEventListener("DOMContentLoaded", () => {
               свежих овощей и фруктов. Продукт активных и здоровых людей. Это
               абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
     9,
-    ".menu-list"
+    ".menu-list",
+    "menu__item",
+    "big"
   ).render();
   new MenuCard(
     "img/tabs/elite.jpg",
